98b2db0c8f5acd05415016dacefbe4d580d0cc3b
NAMAN GUPTA <namangupta4768>
2026-07-27 16:39:13 +0530

    1st pattern question

---
+++ pattern question/1st _pattern.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i ,j ;
+    for(i=0;i<4;i++){
+        for(j=i+1;j>=1;j--){
+            cout<<j ;
+        }
+        cout<<endl ;
+    }
+}
---
+++ pattern_question/first _pattern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i ,j ;
+    for(i=0;i<4;i++){
+        for(j=i+1;j>=1;j--){
+            cout<<j ;
+        }
+        cout<<endl ;
+    }
+    return 0 ;
+}
