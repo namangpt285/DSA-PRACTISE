459c66f2886947c4c99d27faaffcfb06d636ed25
NAMAN GUPTA <namangupta4768>
2026-07-27 17:38:14 +0530

    alphbet pattern question

---
+++ pattern_question/alphabet_pattern.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    for(int j = 0;j<4;j++){
+        for(char i='D'-j;i<='D';i++){
+            cout<<i ;
+        }
+        cout<<endl ;
+    }
+    
+}
