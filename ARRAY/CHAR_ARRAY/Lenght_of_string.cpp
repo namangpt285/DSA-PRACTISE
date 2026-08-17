8351265cd50855f50a06ada57ffaa35b07678cc6
NAMAN GUPTA <namangupta4768@gmail.com>
2026-08-17 17:50:22 +0530

    LENTH OF STRING

---
+++ ARRAY/CHAR_ARRAY/Lenght_of_string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+
+int main(){
+    char name[20];
+    cout<<"ENTER YOUR NAME :";
+    cin>>name ;
+    int length = 0;
+    while(name[length]!=0){
+        length++;
+        
+    }
+    cout<<"LENTH OF STRING IS :"<<length ;
+
+}
